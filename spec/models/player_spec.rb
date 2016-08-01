dabb0070482c1cd79b54d4416c0bffe0432d062e
febbraiod <donedesign1@gmail.com>
2016-08-01 14:51:10 -0400

    add test for validation

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -6,3 +6,12 @@ RSpec.describe Player, type: :model do
   it { should respond_to :name }
   it { should respond_to :salary }
 end
+
+
+describe Player do
+  it "should be invalid without name" do
+    p = Player.new(salary: 1)
+    expect {p.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+end
