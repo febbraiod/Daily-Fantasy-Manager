d6ed7677b8a7f8f2ead42c068802984722a3ac9e
febbraiod <donedesign1@gmail.com>
2016-08-24 16:29:09 -0400

    refactor fd upload

--- app/models/player.rb
+++ app/models/player.rb
@@ -11,20 +11,32 @@ class Player < ActiveRecord::Base
   end
 
   def player_value
-    self.salary/self.projected_points.reduce(0, :+)
+    if self.projected_points.reduce(0, :+) == 0
+      0
+    else
+      self.salary/self.projected_points.reduce(0, :+)
+    end
   end
 
   def self.fd_import(file)
     CSV.foreach(file.tempfile, headers: true, :header_converters => lambda { |h| h.try(:downcase).tr(" ", "_") }) do |row|
+      player_hash = create_player_hash(row)
+      create_player_from_hash(player_hash)
+    end
+  end
+
+  def self.create_player_hash(row)
       player_hash = row.to_hash
       header_map = {"fppg" => "averageppg", "injury_indicator" => "injury_status"}
       player_hash.keys.each { |k| player_hash[ header_map[k] ] = player_hash.delete(k) if header_map[k] }
       player_hash["name"] =  "#{player_hash['first_name']} #{player_hash['last_name']}"
-      player_hash = player_hash.except("first_name", "last_name", "played", "", "game", "id", "injury_details")
-      p = Player.find_or_create_by(name: player_hash[:name])
-      p.update(player_hash)
-      p.save
-    end
+      p = player_hash.except("first_name", "last_name", "played", "", "game", "id", "injury_details")
+  end
+
+  def self.create_player_from_hash(player_hash)
+    p = Player.find_or_create_by(name: player_hash[:name])
+    p.update(player_hash)
+    p.save
   end
 
 end
